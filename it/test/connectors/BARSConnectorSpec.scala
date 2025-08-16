ef79611ac66e1884a3ccf3bc4c9c7bc75cd7d481
javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2025-08-16 18:30:45 +0100

    DTR-79 add tests for BARS connector

---
+++ it/test/connectors/BARSConnectorSpec.scala
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import models.YourBankDetails
+import models.responses.{BarsVerificationResponse, Country}
+import org.scalatest.concurrent.{ScalaFutures, PatienceConfiguration}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.{Millis, Seconds, Span}
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.{BeforeAndAfterAll, OptionValues}
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+import scala.concurrent.ExecutionContext
+
+class BARSConnectorSpec
+  extends AnyWordSpec
+    with Matchers
+    with ScalaFutures
+    with BeforeAndAfterAll
+    with OptionValues {
+
+  private val wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+
+  implicit private val ec: ExecutionContext = ExecutionContext.global
+  implicit private val hc: HeaderCarrier   = HeaderCarrier()
+
+  // Increase patience for futures
+  implicit override val patienceConfig: PatienceConfig =
+    PatienceConfig(timeout = Span(5, Seconds), interval = Span(50, Millis))
+
+  override def beforeAll(): Unit = {
+    wireMockServer.start()
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    wireMockServer.stop()
+    super.afterAll()
+  }
+
+  private val app: Application = new GuiceApplicationBuilder()
+    .configure(
+      "microservice.services.bars.host" -> "localhost",
+      "microservice.services.bars.port" -> wireMockServer.port()
+    )
+    .build()
+
+  private val httpClient: HttpClientV2 = app.injector.instanceOf[HttpClientV2]
+  private val config: ServicesConfig   = app.injector.instanceOf[ServicesConfig]
+
+  private val connector = BARSConnector(config, httpClient)
+
+  private val bankDetails = YourBankDetails(
+    accountHolderName = "Teddy Dickson",
+    sortCode          = "443116",
+    accountNumber     = "55207102"
+  )
+
+  "BARSConnector.verify" should {
+
+    "return verification result with metadata when both endpoints succeed" in {
+      wireMockServer.stubFor(
+        post(urlEqualTo("/verify/personal"))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withBody("""{
+                  "accountNumberIsWellFormatted": "yes",
+                  "sortCodeIsPresentOnEISCD": "yes",
+                  "accountExists": "yes",
+                  "nameMatches": "yes",
+                  "sortCodeSupportsDirectDebit": "yes",
+                  "sortCodeSupportsDirectCredit": "yes"
+                }""")
+          )
+      )
+
+      wireMockServer.stubFor(
+        get(urlEqualTo("/metadata/443116"))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withBody("""{
+                "bankName": "Some Bank",
+                "address": { "country": { "name": "United Kingdom" }, "postCode": "SW1A 1AA" }
+              }""")
+          )
+      )
+
+      val resultF = connector.verify(isPersonal = true, bankDetails)
+
+      whenReady(resultF) { result =>
+        result.accountNumberIsWellFormatted.toString.toLowerCase shouldBe "yes"
+        result.bank.value.name shouldBe "Some Bank"
+        result.bank.value.address.country shouldBe Country("United Kingdom")
+        result.bank.value.address.postCode shouldBe "SW1A 1AA"
+      }
+    }
+
+    "return verification result without bank metadata when metadata endpoint returns 404" in {
+      wireMockServer.stubFor(
+        post(urlEqualTo("/verify/personal"))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withBody("""{
+                  "accountNumberIsWellFormatted": "yes",
+                  "sortCodeIsPresentOnEISCD": "yes",
+                  "accountExists": "yes",
+                  "nameMatches": "yes",
+                  "sortCodeSupportsDirectDebit": "yes",
+                  "sortCodeSupportsDirectCredit": "yes"
+                }""")
+          )
+      )
+
+      wireMockServer.stubFor(
+        get(urlEqualTo("/metadata/443116"))
+          .willReturn(aResponse().withStatus(404))
+      )
+
+      val resultF = connector.verify(isPersonal = true, bankDetails)
+
+      whenReady(resultF) { result =>
+        result.bank shouldBe None
+      }
+    }
+
+    "fail when verification endpoint returns 500" in {
+      wireMockServer.stubFor(
+        post(urlEqualTo("/verify/personal"))
+          .willReturn(aResponse().withStatus(500))
+      )
+
+      val resultF = connector.verify(isPersonal = true, bankDetails)
+
+      whenReady(resultF.failed) { ex =>
+        ex shouldBe a[Exception] // matches actual exception type
+      }
+    }
+
+    "fail when metadata endpoint is called but responds with 500" in {
+      wireMockServer.stubFor(
+        post(urlEqualTo("/verify/personal"))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withBody("""{
+                  "accountNumberIsWellFormatted": "yes",
+                  "sortCodeIsPresentOnEISCD": "yes",
+                  "accountExists": "yes",
+                  "nameMatches": "yes",
+                  "sortCodeSupportsDirectDebit": "yes",
+                  "sortCodeSupportsDirectCredit": "yes"
+                }""")
+          )
+      )
+
+      wireMockServer.stubFor(
+        get(urlEqualTo("/metadata/443116"))
+          .willReturn(aResponse().withStatus(500))
+      )
+
+      val resultF = connector.verify(isPersonal = true, bankDetails)
+
+      whenReady(resultF.failed) { ex =>
+        ex shouldBe a[Exception] // matches actual exception type
+      }
+    }
+  }
+}
