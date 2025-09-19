9a2e7a01f9dff39b68d4d3e7478f7db5434b9b64
sonalgupta1227 <sonal.ab.gupta@digital.hmrc.gov.uk>
2025-09-19 16:01:35 +0100

    DTR-195: test case changes

--- test/controllers/PaymentPlanConfirmationControllerSpec.scala
+++ test/controllers/PaymentPlanConfirmationControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
@@ -15,15 +31,15 @@ class PaymentPlanConfirmationControllerSpec extends SpecBase {
 
     "must return OK and the correct view for a GET" in {
       //TODO: get payment reference from ap2
-//      val userAnswers = emptyUserAnswers
-//        .setOrException(CheckYourAnswerPage, GenerateDdiRefResponse("600002164"))
+      //      val userAnswers = emptyUserAnswers
+      //        .setOrException(CheckYourAnswerPage, GenerateDdiRefResponse("600002164"))
 
       val referenceNumber = "123456789K"
       val paymentAmount: BigDecimal = BigDecimal("1000.00")
       val formattedPaymentAmount: String = NumberFormat.getCurrencyInstance(Locale.UK).format(paymentAmount)
       val startDate: LocalDate = LocalDate.of(2025, 9, 3)
       val formattedStartDate = startDate.format(DateTimeFormatter.ofPattern("d MMMM yyyy"))
-      val endDate: LocalDate = LocalDate.of(2025,11,11)
+      val endDate: LocalDate = LocalDate.of(2025, 11, 11)
       val formattedEndDate = endDate.format(DateTimeFormatter.ofPattern("d MMMM yyyy"))
       val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
 
@@ -35,20 +51,21 @@ class PaymentPlanConfirmationControllerSpec extends SpecBase {
         val view = application.injector.instanceOf[PaymentPlanConfirmationView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(referenceNumber,formattedPaymentAmount,formattedStartDate,formattedEndDate)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(referenceNumber, formattedPaymentAmount, formattedStartDate, formattedEndDate)(request, messages(application)).toString
       }
     }
 
-    "must return error if no payment reference" in {
-
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
-
-      running(application) {
-        val request = FakeRequest(GET, routes.PaymentPlanConfirmationController.onPageLoad().url)
-        val result = intercept[Exception](route(application, request).value.futureValue)
-
-        result.getMessage must include("Missing generated Payment reference number")
-
-      }
+//    "must return error if no payment reference" in {
+//
+//      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+//
+//      running(application) {
+//        val request = FakeRequest(GET, routes.PaymentPlanConfirmationController.onPageLoad().url)
+//        val result = intercept[Exception](route(application, request).value.futureValue)
+//
+//        result.getMessage must include("Missing generated Payment reference number")
+//
+//      }
+//    }
   }
 }
