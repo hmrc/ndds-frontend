5be44a3c99b580c129a67a5488203aa594d632e8
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2025-10-14 14:35:44 +0100

    DTR-203: scalafmt and back link

--- app/viewmodels/checkAnswers/CancelPaymentPlanSummary.scala
+++ app/viewmodels/checkAnswers/CancelPaymentPlanSummary.scala
@@ -21,24 +21,23 @@ import models.{CheckMode, UserAnswers}
 import pages.CancelPaymentPlanPage
 import play.api.i18n.Messages
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
-import viewmodels.govuk.summarylist._
-import viewmodels.implicits._
+import viewmodels.govuk.summarylist.*
+import viewmodels.implicits.*
 
-object CancelPaymentPlanSummary  {
+object CancelPaymentPlanSummary {
 
   def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
-    answers.get(CancelPaymentPlanPage).map {
-      answer =>
+    answers.get(CancelPaymentPlanPage).map { answer =>
 
-        val value = if (answer) "site.yes" else "site.no"
+      val value = if (answer) "site.yes" else "site.no"
 
-        SummaryListRowViewModel(
-          key     = "cancelPaymentPlan.checkYourAnswersLabel",
-          value   = ValueViewModel(value),
-          actions = Seq(
-            ActionItemViewModel("site.change", routes.CancelPaymentPlanController.onPageLoad(CheckMode).url)
-              .withVisuallyHiddenText(messages("cancelPaymentPlan.change.hidden"))
-          )
+      SummaryListRowViewModel(
+        key   = "cancelPaymentPlan.checkYourAnswersLabel",
+        value = ValueViewModel(value),
+        actions = Seq(
+          ActionItemViewModel("site.change", routes.CancelPaymentPlanController.onPageLoad(CheckMode).url)
+            .withVisuallyHiddenText(messages("cancelPaymentPlan.change.hidden"))
         )
+      )
     }
 }
