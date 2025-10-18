2c5f2da470021e223b8e2a379c3d4a88d99238e3
javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2025-10-18 09:40:16 +0100

    DTR-388 initial changes for suspend dates

---
+++ app/controllers/SuspensionPeriodRangeDateController.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions._
+import forms.SuspensionPeriodRangeDateFormProvider
+import javax.inject.Inject
+import models.{Mode, SuspensionPeriodRange}
+import navigation.Navigator
+import pages.SuspensionPeriodRangeDatePage
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.SuspensionPeriodRangeDateView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class SuspensionPeriodRangeDateController @Inject()(
+                                                     override val messagesApi: MessagesApi,
+                                                     sessionRepository: SessionRepository,
+                                                     navigator: Navigator,
+                                                     identify: IdentifierAction,
+                                                     getData: DataRetrievalAction,
+                                                     requireData: DataRequiredAction,
+                                                     formProvider: SuspensionPeriodRangeDateFormProvider,
+                                                     val controllerComponents: MessagesControllerComponents,
+                                                     view: SuspensionPeriodRangeDateView
+                                                   )(implicit ec: ExecutionContext)
+  extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData) { implicit request =>
+      implicit val messages: Messages = messagesApi.preferred(request)
+
+      val form = formProvider()
+
+      val preparedForm = request.userAnswers.get(SuspensionPeriodRangeDatePage) match {
+        case Some(value: SuspensionPeriodRange) => form.fill(value)
+        case _                                  => form
+      }
+
+      Ok(view(preparedForm, mode))
+    }
+
+  def onSubmit(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData).async { implicit request =>
+      implicit val messages: Messages = messagesApi.preferred(request)
+      val form = formProvider()
+
+      form.bindFromRequest().fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode))),
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.set(SuspensionPeriodRangeDatePage, value))
+            _              <- sessionRepository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(SuspensionPeriodRangeDatePage, mode, updatedAnswers))
+      )
+    }
+}
