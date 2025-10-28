e48d2550c0fa26f474fbdad52ea977b3a03d6b90
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2025-10-28 11:11:49 +0000

    [DTR-599] Wrapping form with fieldset

--- test/views/components/FieldSetSpec.scala
+++ test/views/components/FieldSetSpec.scala
@@ -27,65 +27,65 @@ class FieldSetSpec extends SpecBase with Matchers {
   "FieldSet" - {
     "must render with correct legend text" in new Setup {
       val legendText = "Test Legend"
-      val html       = fieldSet(legendText, asHeading = true, describedBy = None)(testContent)
-      val legend     = getLegendElement(html)
+      val html = fieldSet(legendText, asHeading = true, describedBy = None)(testContent)
+      val legend = getLegendElement(html)
       legend.size mustBe 1
       legend.text mustBe legendText
     }
 
     "must render with heading styling when asHeading is true" in new Setup {
-      val html      = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
-      val legend    = getLegendElement(html)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
+      val legend = getLegendElement(html)
       val classAttr = legend.attr("class")
-      classAttr must include("govuk-fieldset__legend--l")
+      classAttr must include("govuk-fieldset__legend--xl govuk-!-margin-bottom-8")
     }
 
     "must not render with heading styling when asHeading is false" in new Setup {
-      val html      = fieldSet("Test Legend", asHeading = false, describedBy = None)(testContent)
-      val legend    = getLegendElement(html)
+      val html = fieldSet("Test Legend", asHeading = false, describedBy = None)(testContent)
+      val legend = getLegendElement(html)
       val classAttr = legend.attr("class")
-      classAttr must not include "govuk-fieldset__legend--l"
+      classAttr must not include "govuk-fieldset__legend--xl"
     }
 
     "must render with isPageHeading attribute when asHeading is true" in new Setup {
-      val html   = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
       val legend = getLegendElement(html)
-      legend.attr("class") must include("govuk-fieldset__legend--l")
+      legend.attr("class") must include("govuk-fieldset__legend--xl govuk-!-margin-bottom-8")
     }
 
     "must not render with isPageHeading attribute when asHeading is false" in new Setup {
-      val html   = fieldSet("Test Legend", asHeading = false, describedBy = None)(testContent)
+      val html = fieldSet("Test Legend", asHeading = false, describedBy = None)(testContent)
       val legend = getLegendElement(html)
-      legend.attr("class") must not include "govuk-fieldset__legend--l"
+      legend.attr("class") must not include "govuk-fieldset__legend--xl"
     }
 
     "must render with describedBy attribute when provided" in new Setup {
       val describedBy = "test-description"
-      val html        = fieldSet("Test Legend", asHeading = true, describedBy = Some(describedBy))(testContent)
-      val fieldset    = getFieldsetElement(html)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = Some(describedBy))(testContent)
+      val fieldset = getFieldsetElement(html)
       fieldset.attr("aria-describedby") mustBe describedBy
     }
 
     "must not render with describedBy attribute when not provided" in new Setup {
-      val html     = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
       val fieldset = getFieldsetElement(html)
       fieldset.hasAttr("aria-describedby") mustBe false
     }
 
     "must render with correct content" in new Setup {
-      val html     = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
       val fieldset = getFieldsetElement(html)
       fieldset.html must include(testContent.body)
     }
 
     "must render with fieldset element" in new Setup {
-      val html     = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
       val fieldset = getFieldsetElement(html)
       fieldset.size mustBe 1
     }
 
     "must render with legend element" in new Setup {
-      val html   = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
+      val html = fieldSet("Test Legend", asHeading = true, describedBy = None)(testContent)
       val legend = getLegendElement(html)
       legend.size mustBe 1
     }
@@ -93,10 +93,10 @@ class FieldSetSpec extends SpecBase with Matchers {
   }
 
   trait Setup {
-    val app                                       = applicationBuilder().build()
-    val fieldSet                                  = app.injector.instanceOf[FieldSet]
-    implicit val request: play.api.mvc.Request[_] = FakeRequest()
-    implicit val messages: Messages               = play.api.i18n.MessagesImpl(
+    val app = applicationBuilder().build()
+    val fieldSet = app.injector.instanceOf[FieldSet]
+    implicit val request: play.api.mvc.Request[?] = FakeRequest()
+    implicit val messages: Messages = play.api.i18n.MessagesImpl(
       play.api.i18n.Lang.defaultLang,
       app.injector.instanceOf[play.api.i18n.MessagesApi]
     )
