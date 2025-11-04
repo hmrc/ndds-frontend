2da36674d10d0e2e167734e5ebab853ccb3e7c90
Javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2025-11-04 14:29:38 +0000

    DTR-849  NDDS Phase 2: Business Functions - F14 - ChRIS submission for Remove PP Suspension Flow (#176)
    DTR-849  NDDS Phase 2: Business Functions - F14 - ChRIS submission for Remove PP Suspension Flow

--- app/controllers/RemovingThisSuspensionController.scala
+++ app/controllers/RemovingThisSuspensionController.scala
@@ -18,15 +18,17 @@ package controllers
 
 import controllers.actions.*
 import forms.RemovingThisSuspensionFormProvider
+import models.requests.ChrisSubmissionRequest
+
 import javax.inject.Inject
-import models.Mode
-import models.responses.PaymentPlanResponse
+import models.{DirectDebitSource, Mode, PaymentPlanType, PlanStartDateDetails, UserAnswers, YourBankDetails, YourBankDetailsWithAuddisStatus}
+import models.responses.{DirectDebitDetails, PaymentPlanResponse}
 import navigation.Navigator
-import pages.RemovingThisSuspensionPage
+import pages.{RemovingThisSuspensionPage, SuspensionPeriodRangeDatePage}
 import play.api.Logging
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import queries.PaymentPlanDetailsQuery
+import queries.{PaymentPlanDetailsQuery, PaymentPlanReferenceQuery}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.RemovingThisSuspensionView
@@ -89,40 +91,131 @@ class RemovingThisSuspensionController @Inject() (
     }
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+  def onSubmit(mode: Mode): Action[AnyContent] =
+    (identify andThen getData andThen requireData).async { implicit request =>
 
-    if (nddsService.suspendPaymentPlanGuard(request.userAnswers)) {
+      if (nddsService.suspendPaymentPlanGuard(request.userAnswers)) {
 
-      request.userAnswers.get(PaymentPlanDetailsQuery) match {
-        case Some(planDetails) =>
-          val planDetail = planDetails.paymentPlanDetails
-          val paymentReference = planDetail.paymentReference
-          val suspensionStartDate = planDetail.suspensionStartDate
-          val suspensionEndDate = planDetail.suspensionEndDate
+        request.userAnswers.get(PaymentPlanDetailsQuery) match {
+          case Some(planDetails) =>
+            val planDetail = planDetails.paymentPlanDetails
+            val paymentReference = planDetail.paymentReference
+            val suspensionStartDate = planDetail.suspensionStartDate
+            val suspensionEndDate = planDetail.suspensionEndDate
 
-          form
-            .bindFromRequest()
-            .fold(
-              formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, paymentReference, suspensionStartDate, suspensionEndDate))),
-              value =>
-                for {
-                  updatedAnswers <- Future.fromTry(request.userAnswers.set(RemovingThisSuspensionPage, value))
-                  _              <- sessionRepository.set(updatedAnswers)
-                } yield Redirect(navigator.nextPage(RemovingThisSuspensionPage, mode, updatedAnswers))
-            )
-        case _ =>
-          Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
-      }
-    } else {
-      request.userAnswers.get(PaymentPlanDetailsQuery) match {
-        case Some(planDetails) =>
-          val errorMessage =
-            s"NDDS Payment Plan Guard: Cannot carry out suspension functionality for this plan type: ${planDetails.paymentPlanDetails.planType}"
-          logger.error(errorMessage)
-          Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
-        case _ =>
-          Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+            form
+              .bindFromRequest()
+              .fold(
+                formWithErrors =>
+                  Future.successful(
+                    BadRequest(view(formWithErrors, mode, paymentReference, suspensionStartDate, suspensionEndDate))
+                  ),
+                value =>
+                  request.userAnswers.get(queries.DirectDebitReferenceQuery) match {
+                    case Some(ddiReference) =>
+                      val chrisRequest = removeSuspensionChrisSubmissionRequest(request.userAnswers, ddiReference)
+                      nddsService.submitChrisData(chrisRequest).flatMap { success =>
+                        if (success) {
+                          logger.info(s"CHRIS submission successful for removing suspension [DDI Ref: $ddiReference]")
+
+                          for {
+                            updatedAnswers <- Future.fromTry(request.userAnswers.set(RemovingThisSuspensionPage, value))
+                            _              <- sessionRepository.set(updatedAnswers)
+                          } yield Redirect(navigator.nextPage(RemovingThisSuspensionPage, mode, updatedAnswers))
+
+                        } else {
+                          logger.error(s"CHRIS submission failed for removing suspension [DDI Ref: $ddiReference]")
+                          Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+                        }
+                      }
+
+                    case None =>
+                      logger.error("Missing DirectDebitReference in UserAnswers when trying to remove suspension")
+                      Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+                  }
+              )
+          case _ =>
+            Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+        }
+
+      } else {
+        request.userAnswers.get(PaymentPlanDetailsQuery) match {
+          case Some(planDetails) =>
+            val errorMessage =
+              s"NDDS Payment Plan Guard: Cannot carry out suspension functionality for this plan type: ${planDetails.paymentPlanDetails.planType}"
+            logger.error(errorMessage)
+            Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+
+          case _ =>
+            Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+        }
       }
     }
+
+  private def removeSuspensionChrisSubmissionRequest(
+    userAnswers: UserAnswers,
+    ddiReference: String
+  ): ChrisSubmissionRequest = {
+    userAnswers.get(PaymentPlanDetailsQuery) match {
+      case Some(response) =>
+        val planDetail = response.paymentPlanDetails
+        val directDebitDetails = response.directDebitDetails
+        val serviceType: DirectDebitSource =
+          DirectDebitSource.objectMap.getOrElse(planDetail.planType, DirectDebitSource.SA)
+
+        val planStartDateDetails: Option[PlanStartDateDetails] = planDetail.scheduledPaymentStartDate.map { date =>
+          PlanStartDateDetails(enteredDate = date, earliestPlanStartDate = date.toString)
+        }
+        val paymentPlanType: PaymentPlanType =
+          PaymentPlanType.values
+            .find(_.toString.equalsIgnoreCase(planDetail.planType))
+            .getOrElse(PaymentPlanType.BudgetPaymentPlan)
+
+        val bankDetailsWithAuddisStatus: YourBankDetailsWithAuddisStatus = buildBankDetailsWithAuddisStatus(directDebitDetails)
+
+        ChrisSubmissionRequest(
+          serviceType                     = serviceType,
+          paymentPlanType                 = paymentPlanType,
+          paymentFrequency                = planDetail.scheduledPaymentFrequency,
+          paymentPlanReferenceNumber      = userAnswers.get(PaymentPlanReferenceQuery),
+          yourBankDetailsWithAuddisStatus = bankDetailsWithAuddisStatus,
+          planStartDate                   = planStartDateDetails,
+          planEndDate                     = planDetail.scheduledPaymentEndDate,
+          paymentDate                     = None,
+          yearEndAndMonth                 = None,
+          ddiReferenceNo                  = ddiReference,
+          paymentReference                = planDetail.paymentReference,
+          totalAmountDue                  = planDetail.totalLiability,
+          paymentAmount                   = planDetail.scheduledPaymentAmount,
+          regularPaymentAmount            = None,
+          amendPaymentAmount              = None,
+          suspensionPeriodRangeDate       = userAnswers.get(SuspensionPeriodRangeDatePage),
+          calculation                     = None,
+          removeSuspensionPlan            = true
+        )
+      case None =>
+        throw new IllegalStateException("Missing PaymentPlanDetails in userAnswers")
+    }
   }
+
+  private def buildBankDetailsWithAuddisStatus(
+    directDebitDetails: DirectDebitDetails
+  ): YourBankDetailsWithAuddisStatus = {
+    val bankDetails = YourBankDetails(
+      accountHolderName = directDebitDetails.bankAccountName.getOrElse(""),
+      sortCode = directDebitDetails.bankSortCode.getOrElse(
+        throw new IllegalStateException("Missing bank sort code")
+      ),
+      accountNumber = directDebitDetails.bankAccountNumber.getOrElse(
+        throw new IllegalStateException("Missing bank account number")
+      )
+    )
+
+    YourBankDetailsWithAuddisStatus.toModelWithAuddisStatus(
+      yourBankDetails = bankDetails,
+      auddisStatus    = directDebitDetails.auDdisFlag,
+      accountVerified = true
+    )
+  }
+
 }
