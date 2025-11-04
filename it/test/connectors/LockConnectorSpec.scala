46043e280fb2c678bb90475ceb557f05d7a31063
HG-Accenture <183600681+HG-Accenture@users.noreply.github.com>
2025-11-04 14:26:24 +0000

    Add integration tests for LockConnector and LockService. Amended run_all_tests.sh.

---
+++ it/test/connectors/LockConnectorSpec.scala
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import models.responses.LockResponse
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.{Millis, Seconds, Span}
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.{BeforeAndAfterAll, OptionValues}
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+import scala.concurrent.ExecutionContext
+
+class LockConnectorSpec
+  extends AnyWordSpec
+    with Matchers
+    with ScalaFutures
+    with BeforeAndAfterAll
+    with OptionValues {
+
+  private val wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+
+  implicit private val ec: ExecutionContext = ExecutionContext.global
+  implicit private val hc: HeaderCarrier = HeaderCarrier()
+
+  implicit override val patienceConfig: PatienceConfig =
+    PatienceConfig(timeout = Span(5, Seconds), interval = Span(50, Millis))
+
+  override def beforeAll(): Unit = {
+    wireMockServer.start()
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    wireMockServer.stop()
+    super.afterAll()
+  }
+
+  lazy private val app: Application = new GuiceApplicationBuilder()
+    .configure(
+      "microservice.services.lock.host" -> "localhost",
+      "microservice.services.lock.port" -> wireMockServer.port()
+    )
+    .build()
+
+  lazy private val httpClient: HttpClientV2 = app.injector.instanceOf[HttpClientV2]
+  lazy private val config: ServicesConfig = app.injector.instanceOf[ServicesConfig]
+  lazy private val connector = LockConnector(config, httpClient)
+
+  private val lockResponseJson =
+    """{
+      |  "_id": "lock-id-1",
+      |  "isLocked": true,
+      |  "unverifiable": true,
+      |  "verifyCalls": 2,
+      |  "createdAt": "2024-01-01T12:00:00Z",
+      |  "lastUpdated": "2024-01-02T12:00:00Z",
+      |  "lockoutExpiryDateTime": "2024-01-10T12:00:00Z"
+      |}""".stripMargin
+
+  "LockConnector" should {
+
+    "return LockResponse from checkLock" in {
+      val credId = "test-cred-id"
+      wireMockServer.stubFor(
+        post(urlEqualTo("/locks/bars/status"))
+          .withRequestBody(equalToJson(s"""{"identifier": "$credId"}"""))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withHeader("Content-Type", "application/json")
+              .withBody(lockResponseJson)
+          )
+      )
+
+      val result = connector.checkLock(credId).futureValue
+      result._id shouldBe "lock-id-1"
+      result.isLocked shouldBe true
+      result.unverifiable.value shouldBe true
+      result.verifyCalls shouldBe 2
+    }
+
+    "return LockResponse from updateLock" in {
+      val credId = "test-cred-id"
+      wireMockServer.stubFor(
+        post(urlEqualTo("/locks/bars/update"))
+          .withRequestBody(equalToJson(s"""{"identifier": "$credId"}"""))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withHeader("Content-Type", "application/json")
+              .withBody(lockResponseJson)
+          )
+      )
+
+      val result = connector.updateLock(credId).futureValue
+      result._id shouldBe "lock-id-1"
+      result.isLocked shouldBe true
+    }
+
+    "return LockResponse from markUnverifiable" in {
+      val credId = "test-cred-id"
+      wireMockServer.stubFor(
+        post(urlEqualTo("/locks/bars/markUnverifiable"))
+          .withRequestBody(equalToJson(s"""{"identifier": "$credId"}"""))
+          .willReturn(
+            aResponse()
+              .withStatus(200)
+              .withHeader("Content-Type", "application/json")
+              .withBody(lockResponseJson)
+          )
+      )
+
+      val result = connector.markUnverifiable(credId).futureValue
+      result.unverifiable.value shouldBe true
+    }
+
+    "fail when service returns error" in {
+      val credId = "test-cred-id"
+      wireMockServer.stubFor(
+        post(urlEqualTo("/locks/bars/status"))
+          .withRequestBody(equalToJson(s"""{"identifier": "$credId"}"""))
+          .willReturn(aResponse().withStatus(500).withBody("error"))
+      )
+
+      val ex = intercept[Exception] {
+        connector.checkLock(credId).futureValue
+      }
+      ex.getMessage should include("500")
+    }
+  }
+}
