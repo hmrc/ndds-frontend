5107f985385c2d8a73b680d069fe3ef314a4d01a
sonalgupta1227 <sonal.ab.gupta@digital.hmrc.gov.uk>
2025-11-05 13:40:06 +0000

    DTR-585: Tax credit plan label changes

--- app/controllers/DirectDebitSummaryController.scala
+++ app/controllers/DirectDebitSummaryController.scala
@@ -125,15 +125,7 @@ class DirectDebitSummaryController @Inject() (
             optionalRow(Option(plan.submissionDateTime))(v => DateSetupSummary.row(v))
           ).flatten
 
-        case PaymentPlanType.TaxCreditRepaymentPlan.toString =>
-          Seq(
-            optionalRow(Option(plan.planType))(v => AmendPaymentPlanTypeSummary.row(v)),
-            optionalRow(Option(plan.hodService))(v => AmendPaymentPlanSourceSummary.row(v)),
-            optionalRow(Option(plan.submissionDateTime))(v => DateSetupSummary.row(v))
-            // optionalRow(Option(plan.totalAmountDue))(v => TotalAmountDueSummary.row(v))
-          ).flatten
-
-        case _ => // For Single and Budget plan
+        case _ => // For Single, TaxCreditRepaymentPlan and Budget plan
           Seq(
             optionalRow(Option(plan.planType))(v => AmendPaymentPlanTypeSummary.row(v)),
             optionalRow(Option(plan.hodService))(v => AmendPaymentPlanSourceSummary.row(v)),
