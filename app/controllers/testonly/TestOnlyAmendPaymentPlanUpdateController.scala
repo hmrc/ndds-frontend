b939451e3e3c0dce82d77359ae3c0ba88a4a94cd
sonalgupta1227 <sonal.ab.gupta@digital.hmrc.gov.uk>
2025-11-21 17:21:52 +0000

    DTR-588: AP3 Screen model

---
+++ app/controllers/testonly/TestOnlyAmendPaymentPlanUpdateController.scala
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.testonly
+
+import controllers.routes
+import controllers.actions.*
+import models.{PaymentPlanType, UserAnswers}
+import pages.*
+import play.api.Logging
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import queries.PaymentPlanDetailsQuery
+import services.NationalDirectDebitService
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.Constants
+import utils.MaskAndFormatUtils.formatAmount
+import viewmodels.checkAnswers.*
+import views.html.TestOnlyPaymentPlanUpdateView
+
+import java.time.format.DateTimeFormatter
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class TestOnlyAmendPaymentPlanUpdateController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  nddsService: NationalDirectDebitService,
+  val controllerComponents: MessagesControllerComponents,
+  view: TestOnlyPaymentPlanUpdateView
+) extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    val userAnswers = request.userAnswers
+    if (nddsService.amendPaymentPlanGuard(userAnswers)) {
+      val maybeResult = for {
+        paymentPlan   <- userAnswers.get(PaymentPlanDetailsQuery)
+        paymentAmount <- userAnswers.get(AmendPaymentAmountPage)
+        startDate     <- userAnswers.get(AmendPlanStartDatePage)
+      } yield {
+        val formattedRegPaymentAmount = formatAmount(paymentAmount)
+        val formattedStartDate =
+          startDate.format(DateTimeFormatter.ofPattern(Constants.longDateTimeFormatPattern))
+        val paymentReference = paymentPlan.paymentPlanDetails.paymentReference
+        val summaryRows: Seq[SummaryListRow] = buildSummaryRows(userAnswers, paymentReference)
+
+        Ok(view(paymentReference, formattedRegPaymentAmount, formattedStartDate, summaryRows))
+      }
+      maybeResult match {
+        case Some(result) => Future.successful(result)
+        case None =>
+          logger.warn("Missing required values in user answers for amend payment plan")
+          Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+      }
+    } else {
+      val planType = request.userAnswers.get(ManagePaymentPlanTypePage).getOrElse("")
+      logger.error(s"NDDS Payment Plan Guard: Cannot amend this plan type: $planType")
+      Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+    }
+  }
+
+  private def buildSummaryRows(userAnswers: UserAnswers, paymentPlanReference: String)(implicit messages: Messages): Seq[SummaryListRow] = {
+
+    val planTypeOpt = userAnswers.get(ManagePaymentPlanTypePage)
+    val paymentAmount = userAnswers.get(AmendPaymentAmountPage)
+    val planStartDate = userAnswers.get(AmendPlanStartDatePage)
+    val planEndDate = userAnswers.get(AmendPlanEndDatePage)
+
+    planTypeOpt match {
+      case Some(PaymentPlanType.SinglePaymentPlan.toString) =>
+        Seq(
+          PaymentReferenceSummary.row(paymentPlanReference),
+          AmendPaymentAmountSummary.row(PaymentPlanType.SinglePaymentPlan.toString, paymentAmount),
+          AmendPlanStartDateSummary.row(PaymentPlanType.SinglePaymentPlan.toString, planStartDate, Constants.longDateTimeFormatPattern)
+        )
+      case Some(PaymentPlanType.BudgetPaymentPlan.toString) =>
+        Seq(
+          PaymentReferenceSummary.row(paymentPlanReference),
+          AmendPaymentAmountSummary.row(PaymentPlanType.BudgetPaymentPlan.toString, paymentAmount),
+          AmendPlanStartDateSummary.row(PaymentPlanType.BudgetPaymentPlan.toString, planStartDate, Constants.longDateTimeFormatPattern),
+          AmendPlanEndDateSummary.row(planEndDate, Constants.longDateTimeFormatPattern)
+        )
+      case _ => throw new IllegalStateException("Plan type is missing or invalid")
+    }
+  }
+}
