cf93d61171d4c37c19439ae51c68ac04ecdf5131
Sonal Gupta <sonal.ab.gupta@digital.hmrc.gov.uk>
2025-11-27 14:54:52 +0000

    DTR-1429: Screen AP3 Budget Payment Plan (#269)

--- test/controllers/testonly/TestOnlyAmendPaymentPlanUpdateControllerSpec.scala
+++ test/controllers/testonly/TestOnlyAmendPaymentPlanUpdateControllerSpec.scala
@@ -17,14 +17,16 @@
 package controllers.testonly
 
 import base.SpecBase
-import config.FrontendAppConfig
-import models.PaymentPlanType
 import models.responses.{DirectDebitDetails, PaymentPlanDetails, PaymentPlanResponse}
+import config.FrontendAppConfig
+import models.{PaymentPlanType, UserAnswers}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar.mock
-import pages.{AmendPaymentAmountPage, AmendPlanStartDatePage, ManagePaymentPlanTypePage}
+import pages.{AmendPaymentAmountPage, AmendPlanEndDatePage, AmendPlanStartDatePage, ManagePaymentPlanTypePage}
 import play.api.inject
+
+import java.time.{LocalDate, LocalDateTime}
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import queries.{DirectDebitReferenceQuery, PaymentPlanDetailsQuery, PaymentPlanReferenceQuery}
@@ -33,15 +35,22 @@ import views.html.testonly.TestOnlyAmendPaymentPlanUpdateView
 
 import java.text.NumberFormat
 import java.time.format.DateTimeFormatter
-import java.time.{LocalDate, LocalDateTime}
 import java.util.Locale
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import utils.Constants
+import utils.MaskAndFormatUtils.formatAmount
+import viewmodels.checkAnswers.{AmendPaymentAmountSummary, AmendPlanEndDateSummary, AmendPlanStartDateSummary, DateSetupSummary, PaymentReferenceSummary, PaymentsFrequencySummary}
+import viewmodels.govuk.all.SummaryListViewModel
 
 class TestOnlyAmendPaymentPlanUpdateControllerSpec extends SpecBase {
 
   "PaymentPlanConfirmation Controller" - {
     val mockService = mock[NationalDirectDebitService]
     val regPaymentAmount: BigDecimal = BigDecimal("1000.00")
+    val formattedRegPaymentAmount: String = formatAmount(regPaymentAmount)
     val startDate: LocalDate = LocalDate.of(2025, 10, 2)
+    val formattedStartDate = startDate.format(DateTimeFormatter.ofPattern("d MMMM yyyy"))
+    val endDate: LocalDate = LocalDate.of(2025, 10, 25)
 
     "must return OK and the correct view for a GET when plan type is Single Payment Plan" in {
 
@@ -80,6 +89,7 @@ class TestOnlyAmendPaymentPlanUpdateControllerSpec extends SpecBase {
         .build()
 
       running(application) {
+
         when(mockService.amendPaymentPlanGuard(any())).thenReturn(true)
 
         val controller = application.injector.instanceOf[TestOnlyAmendPaymentPlanUpdateController]
@@ -90,34 +100,153 @@ class TestOnlyAmendPaymentPlanUpdateControllerSpec extends SpecBase {
         val appConfig = application.injector.instanceOf[FrontendAppConfig]
 
         val currencyFormat = NumberFormat.getCurrencyInstance(Locale.UK)
-        val dateFormat = DateTimeFormatter.ofPattern("d MMMM yyyy")
-        val dateFormatMonth = DateTimeFormatter.ofPattern("d MMM yyyy")
+        val dateFormatLong = DateTimeFormatter.ofPattern("d MMMM yyyy")
+        val dateFormatShort = DateTimeFormatter.ofPattern("d MMM yyyy")
 
         val formattedRegPaymentAmount = currencyFormat.format(regPaymentAmount)
-        val formattedStartDate = dateFormat.format(startDate)
-        val formattedStartDateMonth = dateFormatMonth.format(startDate)
-        val formattedSubmissionDate =
-          dateFormatMonth.format(mockSinglePaymentPlanDetailResponse.paymentPlanDetails.submissionDateTime)
+        val formattedStartDateLong = dateFormatLong.format(startDate)
+        val formattedStartDateShort = dateFormatShort.format(startDate)
+        val formattedSubmissionDate = dateFormatShort.format(mockSinglePaymentPlanDetailResponse.paymentPlanDetails.submissionDateTime)
 
         val dd = mockSinglePaymentPlanDetailResponse.directDebitDetails
-        val formattedSortCode = dd.bankSortCode
-          .map(sc => sc.grouped(2).mkString(" "))
-          .getOrElse("")
-        val paymentReference = mockSinglePaymentPlanDetailResponse.paymentPlanDetails.paymentReference
+        val formattedSortCode = dd.bankSortCode.map(sc => sc.grouped(2).mkString(" ")).getOrElse("")
+
+        val paymentList =
+          SummaryListViewModel(
+            Seq(
+              PaymentReferenceSummary.row(paymentPlanRef)(messages(application)),
+              DateSetupSummary.row(mockSinglePaymentPlanDetailResponse.paymentPlanDetails.submissionDateTime)(messages(application)),
+              AmendPaymentAmountSummary.row(
+                PaymentPlanType.SinglePaymentPlan.toString,
+                Some(regPaymentAmount)
+              )(messages(application)),
+              AmendPlanStartDateSummary.row(
+                PaymentPlanType.SinglePaymentPlan.toString,
+                Some(startDate),
+                Constants.shortDateTimeFormatPattern
+              )(messages(application))
+            )
+          )
 
         status(result) mustEqual OK
 
         contentAsString(result) mustEqual view(
           appConfig.hmrcHelplineUrl,
           formattedRegPaymentAmount,
-          formattedStartDate,
+          formattedStartDateLong,
           directDebitRef,
           dd.bankAccountName.getOrElse(""),
           dd.bankAccountNumber.getOrElse(""),
           formattedSortCode,
-          paymentReference,
-          formattedSubmissionDate,
-          formattedStartDateMonth,
+          paymentList,
+          PaymentPlanType.SinglePaymentPlan.toString,
+          controllers.routes.PaymentPlanDetailsController.onPageLoad()
+        )(request, messages(application)).toString
+      }
+    }
+
+    "must return OK and the correct view for a GET when plan type is Budget Payment Plan" in {
+
+      val frequency = "monthly"
+
+      val mockBudgetPaymentPlanDetailResponse =
+        dummyPlanDetailResponse.copy(
+          paymentPlanDetails = dummyPlanDetailResponse.paymentPlanDetails.copy(
+            planType                  = PaymentPlanType.BudgetPaymentPlan.toString,
+            scheduledPaymentFrequency = Some(frequency)
+          )
+        )
+
+      val directDebitRef = "DD-REF-123"
+      val paymentPlanRef = "PP-REF-987"
+
+      val userAnswers = emptyUserAnswers
+        .set(PaymentPlanDetailsQuery, mockBudgetPaymentPlanDetailResponse)
+        .success
+        .value
+        .set(AmendPaymentAmountPage, regPaymentAmount)
+        .success
+        .value
+        .set(AmendPlanStartDatePage, startDate)
+        .success
+        .value
+        .set(AmendPlanEndDatePage, endDate)
+        .success
+        .value
+        .set(ManagePaymentPlanTypePage, PaymentPlanType.BudgetPaymentPlan.toString)
+        .success
+        .value
+        .set(DirectDebitReferenceQuery, directDebitRef)
+        .success
+        .value
+        .set(PaymentPlanReferenceQuery, paymentPlanRef)
+        .success
+        .value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers))
+        .overrides(inject.bind[NationalDirectDebitService].toInstance(mockService))
+        .build()
+
+      running(application) {
+
+        when(mockService.amendPaymentPlanGuard(any())).thenReturn(true)
+
+        val controller = application.injector.instanceOf[TestOnlyAmendPaymentPlanUpdateController]
+        val request = FakeRequest(GET, routes.TestOnlyAmendPaymentPlanUpdateController.onPageLoad().url)
+        val result = controller.onPageLoad()(request)
+
+        val view = application.injector.instanceOf[TestOnlyAmendPaymentPlanUpdateView]
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+
+        val currencyFormat = NumberFormat.getCurrencyInstance(Locale.UK)
+        val dateFormatLong = DateTimeFormatter.ofPattern("d MMMM yyyy")
+        val dateFormatShort = DateTimeFormatter.ofPattern("d MMM yyyy")
+
+        val formattedRegPaymentAmount = currencyFormat.format(regPaymentAmount)
+        val formattedStartDateLong = dateFormatLong.format(startDate)
+        val formattedStartDateShort = dateFormatShort.format(startDate)
+        val formattedSubmissionDate = dateFormatShort.format(
+          mockBudgetPaymentPlanDetailResponse.paymentPlanDetails.submissionDateTime
+        )
+
+        val dd = mockBudgetPaymentPlanDetailResponse.directDebitDetails
+        val formattedSortCode = dd.bankSortCode.map(sc => sc.grouped(2).mkString(" ")).getOrElse("")
+
+        val paymentList =
+          SummaryListViewModel(
+            Seq(
+              PaymentReferenceSummary.row(paymentPlanRef)(messages(application)),
+              DateSetupSummary.row(mockBudgetPaymentPlanDetailResponse.paymentPlanDetails.submissionDateTime)(messages(application)),
+              AmendPaymentAmountSummary.row(
+                PaymentPlanType.BudgetPaymentPlan.toString,
+                Some(regPaymentAmount)
+              )(messages(application)),
+              PaymentsFrequencySummary.row(Some(frequency))(messages(application)),
+              AmendPlanStartDateSummary.row(
+                PaymentPlanType.BudgetPaymentPlan.toString,
+                Some(startDate),
+                Constants.shortDateTimeFormatPattern
+              )(messages(application)),
+              AmendPlanEndDateSummary.row(
+                Some(endDate),
+                Constants.shortDateTimeFormatPattern,
+                false
+              )(messages(application))
+            )
+          )
+
+        status(result) mustEqual OK
+
+        contentAsString(result) mustEqual view(
+          appConfig.hmrcHelplineUrl,
+          formattedRegPaymentAmount,
+          formattedStartDateLong,
+          directDebitRef,
+          dd.bankAccountName.getOrElse(""),
+          dd.bankAccountNumber.getOrElse(""),
+          formattedSortCode,
+          paymentList,
+          PaymentPlanType.BudgetPaymentPlan.toString,
           controllers.routes.PaymentPlanDetailsController.onPageLoad()
         )(request, messages(application)).toString
       }
