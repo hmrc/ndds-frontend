ed01d834d2ab6bd76c2241c5b4d984fa5ebc25ae
shridhar-chippa <189346280+shridhar-chippa@users.noreply.github.com>
2026-03-18 13:22:19 +0000

    refactored code

--- app/connectors/NationalDirectDebitConnector.scala
+++ app/connectors/NationalDirectDebitConnector.scala
@@ -77,23 +77,6 @@ class NationalDirectDebitConnector @Inject() (config: ServicesConfig, http: Http
       }
   }
 
-//  def generateNewDdiReference(generateDdiRefRequest: GenerateDdiRefRequest)(implicit hc: HeaderCarrier): Future[GenerateDdiRefResponse] = {
-//    http
-//      .post(url"$nationalDirectDebitBaseUrl/direct-debit-reference")
-//      .withBody(Json.toJson(generateDdiRefRequest))
-//      .execute[Either[UpstreamErrorResponse, HttpResponse]]
-//      .flatMap {
-//        case Right(response) if response.status == OK =>
-//          Try(response.json.as[GenerateDdiRefResponse]) match {
-//            case Success(data)      => Future.successful(data)
-//            case Failure(exception) => Future.failed(new Exception(s"Invalid JSON format $exception"))
-//          }
-//        case Left(errorResponse) =>
-//          Future.failed(new Exception(s"Unexpected response: ${errorResponse.message}, status code: ${errorResponse.statusCode}"))
-//        case Right(response) => Future.failed(new Exception(s"Unexpected status code: ${response.status}"))
-//      }
-//  }
-
   def generateNewDdiReference(generateDdiRefRequest: GenerateDdiRefRequest)(implicit hc: HeaderCarrier): Future[GenerateDdiRefResponse] = {
     http
       .post(url"$nationalDirectDebitBaseUrl/direct-debit-reference")
