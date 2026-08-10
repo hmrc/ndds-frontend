01de56e7e7d181ad7be086cc4f53c713a245a4f0
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2026-08-10 16:33:14 +0100

    remove println

--- app/controllers/PaymentReferenceController.scala
+++ app/controllers/PaymentReferenceController.scala
@@ -90,7 +90,6 @@ class PaymentReferenceController @Inject() (
           Future.successful(Redirect(navigator.nextPage(PaymentReferencePage, mode, answers)))
         case Some(serviceType) =>
           val form = formProvider(selectedAnswers, Some(serviceType))
-          println(form)
           form
             .bindFromRequest()
             .fold(
